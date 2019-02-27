77ab25359aa96d50ec83c5fffb96b4a32b5ad196
denisijcu <denisijcu266@gmail.com>
2019-02-26 21:17:46 -0500

    first commit

---
+++ src/Messaje.js
@@ -0,0 +1,10 @@
+import React from 'react';
+function Avatar(){
+    return(
+        <div className = 'message'>
+        <h1> This is a message in a tweet Message</h1>
+        </div>
+    )
+}
+
+export default Avatar;
---
+++ src/NamewithHandle.js
@@ -0,0 +1,10 @@
+import React from 'react';
+function NamewithHandle(props){
+    return(
+        <div className = "name">
+        <h1> Name: {props.name}</h1>
+        </div>
+    )
+}
+
+export default NamewithHandle;
